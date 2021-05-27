3f3c0836e96f6057defa268460915628cbb61449
Aaron Geller <a-geller@northwestern.edu>
2021-05-27 10:52:40 -0500

    added basic movement for cardboard

--- static/js/viewer/initViewer.js
+++ static/js/viewer/initViewer.js
@@ -21,6 +21,7 @@ function initControls(){
 		viewerParams.controls.addEventListener('change', sendCameraInfoToGUI);
 	} else if (viewerParams.useOrientationControls) {
 		viewerParams.controls = new THREE.DeviceOrientationControls(viewerParams.camera);
+		viewerParams.controls.updateAlphaOffsetAngle(THREE.Math.degToRad(-90));
 	} else {
 		viewerParams.controls = new THREE.FlyControls( viewerParams.camera , viewerParams.renderer.domElement);
 		viewerParams.controls.movementSpeed = 1. - Math.pow(viewerParams.friction, viewerParams.flyffac);
--- static/lib/DeviceOrientationControls.js
+++ static/lib/DeviceOrientationControls.js
@@ -1,6 +1,7 @@
 /**
  * @author richt / http://richt.me
  * @author WestLangley / http://github.com/WestLangley
+ * updates from Aaron M Geller (AMG)
  *
  * W3C Device Orientation control (http://w3c.github.io/deviceorientation/spec-source-orientation.html)
  */
@@ -22,10 +23,25 @@ THREE.DeviceOrientationControls = function( object ) {
 	this.betaOffsetAngle = 0;
 	this.gammaOffsetAngle = 0;
 
+	//added by AMG
+	this.deviceMotion = {};
+	// store every two acceleration and implied velocity and position,
+	// starting from these initial conditions
+	this.accPosition = [
+		{
+			position: new THREE.Vector3(0,0,0),
+			velocity: new THREE.Vector3(0,0,0),
+			acceleration: new THREE.Vector3(0,0,0),
+			time: undefined
+		}
+	];
+	this.moveMult = new THREE.Vector3(25,10,10);
+
 
 	var onDeviceOrientationChangeEvent = function( event ) {
 
 		scope.deviceOrientation = event;
+
 
 	};
 
@@ -34,6 +50,13 @@ THREE.DeviceOrientationControls = function( object ) {
 		scope.screenOrientation = window.orientation || 0;
 
 	};
+
+
+	var onDeviceMotionChangeEvent = function(){
+		//added by AMG 
+		scope.deviceMotion = event;
+
+	}
 
 	// The angles alpha, beta and gamma form a set of intrinsic Tait-Bryan angles of type Z-X'-Y''
 
@@ -61,12 +84,71 @@ THREE.DeviceOrientationControls = function( object ) {
 
 	}();
 
+	//added by AMG to allow movement in position
+	// https://bl.ocks.org/micahstubbs/0d7ac58c57c9cd663de5ae136e8bc405
+	// euler double integration
+	var eulerStep = function(state0, state1) {
+		var dt = (state1.time - state0.time) / 1000; // convert ms to s
+		if (dt) {
+			state1.position.x = state0.position.x + state0.velocity.x*dt;
+			state1.velocity.x = state0.velocity.x + state0.acceleration.x*dt;
+			
+			state1.position.y = state0.position.y + state0.velocity.y*dt;
+			state1.velocity.y = state0.velocity.y + state0.acceleration.y*dt;
+
+			state1.position.z = state0.position.z + state0.velocity.z*dt;
+			state1.velocity.z = state0.velocity.z + state0.acceleration.z*dt;
+		}
+		return Object.assign({}, state1);
+	};
+	// step forward with new acceleration, applying some very crude filtering & friction
+	this.accelerate = function(a, t) {
+
+		var newPosition = {
+			position: new THREE.Vector3(0,0,0),
+			velocity: new THREE.Vector3(0,0,0),
+			acceleration: new THREE.Vector3(0,0,0),
+			time: t
+		}
+		newPosition = eulerStep(this.accPosition[0], newPosition);
+		newPosition.acceleration.x = Math.abs(a.x) > .1 ? a.x : 0; // noise filter
+		newPosition.acceleration.y = Math.abs(a.y) > .1 ? a.y : 0; // noise filter
+		newPosition.acceleration.z = Math.abs(a.z) > .1 ? a.z : 0; // noise filter
+
+		newPosition.velocity.multiplyScalar(.9); // friction
+		newPosition.velocity.x = Math.abs(newPosition.velocity.x) < .01 ? 0 : newPosition.velocity.x; // noise filter
+		newPosition.velocity.y = Math.abs(newPosition.velocity.y) < .01 ? 0 : newPosition.velocity.y; // noise filter
+		newPosition.velocity.z = Math.abs(newPosition.velocity.z) < .01 ? 0 : newPosition.velocity.z; // noise filter
+		//newPosition.position.multiplyScalar(.999); // tend back to zero
+
+		this.accPosition.unshift(newPosition);
+
+
+		if (this.accPosition.length >= 2) {
+			//added by AMG to apply the move
+			var dPosition = this.accPosition[1].position.clone();
+			dPosition.sub(this.accPosition[0].position);
+			dPosition.multiplyScalar(moveMult);
+
+			//console.log(dPosition, this.accPosition[1].position, this.accPosition[0].position)
+			this.object.translateX( dPosition.x*this.moveMult.x );
+			this.object.translateY( dPosition.y*this.moveMult.y );
+			this.object.translateZ( dPosition.z*this.moveMult.z );
+			//added by AMG to remove the last z so that the array doesn't get too long
+			if (this.accPosition.length > 2) this.accPosition.pop();
+		}
+
+
+	};
+
 	this.connect = function() {
 
 		onScreenOrientationChangeEvent(); // run once on load
 
 		window.addEventListener( 'orientationchange', onScreenOrientationChangeEvent, false );
 		window.addEventListener( 'deviceorientation', onDeviceOrientationChangeEvent, false );
+		//added by AMG
+		window.addEventListener( 'devicemotion', onDeviceMotionChangeEvent, false );
 
 		scope.enabled = true;
 
@@ -76,10 +158,13 @@ THREE.DeviceOrientationControls = function( object ) {
 
 		window.removeEventListener( 'orientationchange', onScreenOrientationChangeEvent, false );
 		window.removeEventListener( 'deviceorientation', onDeviceOrientationChangeEvent, false );
+		//added by AMG
+		window.removeEventListener( 'devicemotion', onDeviceMotionChangeEvent, false );
 
 		scope.enabled = false;
 
 	};
+
 
 	this.update = function() {
 
@@ -89,9 +174,11 @@ THREE.DeviceOrientationControls = function( object ) {
 		var beta = scope.deviceOrientation.beta ? THREE.Math.degToRad( scope.deviceOrientation.beta ) + this.betaOffsetAngle : 0; // X'
 		var gamma = scope.deviceOrientation.gamma ? THREE.Math.degToRad( scope.deviceOrientation.gamma ) + this.gammaOffsetAngle : 0; // Y''
 		var orient = scope.screenOrientation ? THREE.Math.degToRad( scope.screenOrientation ) : 0; // O
-
 		setObjectQuaternion( scope.object.quaternion, alpha, beta, gamma, orient );
 		this.alpha = alpha;
+
+
+		if ('acceleration' in scope.deviceMotion)this.accelerate(scope.deviceMotion.acceleration, scope.deviceMotion.timeStamp);
 
 	};
 
@@ -116,6 +203,13 @@ THREE.DeviceOrientationControls = function( object ) {
 
 	};
 
+	//added by AMG
+	this.updateMoveMult = function( mult ) {
+
+		this.moveMult = mult;
+		this.update();
+
+	};
 	this.dispose = function() {
 
 		this.disconnect();
