ba946f8ffe04453dc275524a09a631b7ce068d69
Alex Gurvich <agurvich@u.northwestern.edu>
2021-06-28 21:08:05 -0500

    clean up tween params and add docstrings

--- src/Firefly/data_reader/tween.py
+++ src/Firefly/data_reader/tween.py
@@ -10,41 +10,40 @@ class TweenParams(object):
     """A class to store tween parameters and make an output file"""
     def __init__(
         self,
-        coordss = None,
-        durations = None,
-        loop = True
-        ):
-        """ 
-        Create a new tween parameter object. 
-        Input:  
-            coordss - 
-            durations - 
-            loop -
+        coords=None,
+        duration=5,
+        loop=True):
+        """Create a new tween parameter object, allowing the user to press :code:`t` from within
+            the webapp to move between keyframe camera locations smoothly and automatically.
+
+        :param coords: keyframe camera coordinates, list of positions that camera
+            will move between. 3 acceptable input formats:
+            * [x,y,z] single keyframe
+            * [[x1,y1,z1],[x2,y2,z2],...] multiple keyframes
+            * [x1,y1,z1,x2,y2,z2,...] multiple flattened keyframes,
+            defaults to []
+        :type coords: list of float
+        :param duration: duration to approach keyframe in seconds. 3 acceptable input formats:
+            * d single duration (will be repeated)
+            * [d] single duration in list (will be repeated)
+            * [d1,d2,...] multiple durations (corresponding to number of keyframes or
+                raises an error),
+            defaults to 5
+        :type duration: float/list of float
+        :param loop: flag to loop after reaching the last keyframe, defaults to True
+        :type loop: bool, optional
         """
 
-        if coordss is None:
-            coordss = np.array([]).reshape(0,3)
-        else:
-            coordss = np.array(coordss)
+        ## initialize containers
+        self.coordss = np.array([]).reshape(0,3)
+        self.durations = np.array([])
 
-        if durations is None:
-            durations = np.array([])
-        else:
-            durations = np.array(durations)
-
-        if durations.shape[0]!=coordss.shape[0]:
-            raise FireflyError(
-                "Mismatching coordss and durations shape (%d,%d)"%(
-                    coords.shape[0],
-                    durations.shape[0]))
-        if coordss.shape[-1] != 3:
-            raise FireflyError(
-                "coordss should be an Nx3 array of x,y,z positions"+
-                " currently it has shape: "+str(coordss.shape))
-
-        self.coordss = coordss
-        self.durations = durations
+        ## store loop flag
         self.loop = bool(loop)
+        
+        ## add keyframes if any were passed
+        if coords is not None:
+            self.addKeyframe(coords,duration)
 
     def addKeyframe(
         self,
@@ -52,16 +51,21 @@ class TweenParams(object):
         duration):
         """ 
         Adds a new keyframe to an existing TweenParams object. 
-        Input:  
-            coords - array of coordinates, 3 settings for type of input:
+
+        :param coords: keyframe camera coordinates, list of positions that camera
+            will move between 3 acceptable input formats:
                 * [x,y,z] single keyframe
                 * [[x1,y1,z1],[x2,y2,z2],...] multiple keyframes
                 * [x1,y1,z1,x2,y2,z2,...] multiple flattened keyframes
-            durations - duration to approach keyframe, 3 settings for type of input:
-                * d single duration
-                * [d] single duration in list
+        :type coords: list of float 
+        :param duration: duration to approach keyframe, 3 acceptable input formats:
+                * d single duration (will be repeated)
+                * [d] single duration in list (will be repeated)
                 * [d1,d2,...] multiple durations (corresponding to number of keyframes or
                     raises an error)
+        :type duration: float/list of float 
+        :raises np.AxisError: if len of coords is not divisible by 3
+        :raises np.AxisError: if len of durations does not match len of coords
         """
 
         try:
@@ -69,14 +73,17 @@ class TweenParams(object):
             ##  convenient input
             coords = np.array(coords).reshape(-1,3)
         except:
-            raise FireflyError("coords should either be a 2d Nx3 numpy array or"+
+            raise np.AxisError("coords should either be a 2d Nx3 numpy array or"+
                 "a 3N list/array.")
 
         ## convert duration to a 1d numpy array, however it was passed
         duration = np.array(duration).reshape(-1)
+        if duration.shape == 1: duration = np.repeat(duration,coords.shape[0])
 
+        ## ensure there is a duration per keyframe transition
+        ##  TODO: shouldn't durations be 1 less than coordss?
         if duration.shape[0]!=coords.shape[0]:
-            raise FireflyError(
+            raise np.AxisError(
                 "Mismatching coords and duration shape (%d,%d)"%(
                     coords.shape[0],
                     duration.shape[0]))
@@ -85,6 +92,11 @@ class TweenParams(object):
         self.durations = np.append(self.durations,duration)
 
     def outputToDict(self):
+        """Converts stored data into a single python dictionary.
+
+        :return: tween_params_dict
+        :rtype: dict
+        """
         xs,ys,zs = self.coordss.T
         keyframe_dicts = [
             {'x':xs[i],'y':ys[i],'z':zs[i]} 
@@ -101,10 +113,34 @@ class TweenParams(object):
     def outputToJSON(
         self,
         JSONdir,
-        filename=None,
         JSON_prefix='',
+        filename=None,
         loud=1,
         write_jsons_to_disk=True):
+        """ Saves the current tween parameters to a JSON file.
+
+        :param JSONdir: the sub-directory that will contain your JSON files, relative
+            to your :code:`$HOME directory`. , defaults to :code:`$HOME/<JSON_prefix>`
+        :type JSONdir: str, optional
+        :param JSON_prefix: Prefix for any :code:`.json` files created, :code:`.json` files will be of the format:
+            :code:`<JSON_prefix><filename>.json`, defaults to 'Data'
+        :type JSON_prefix: str, optional
+        :param filename: name of settings :code:`.json` file,
+            defaults to self.settings_filename
+        :type filename: str, optional
+        :param JSON_prefix: string that is prepended to filename, defaults to ''
+        :type JSON_prefix: str, optional
+        :param loud: flag to print status information to the console, defaults to True
+        :type loud: bool, optional
+        :param write_jsons_to_disk: flag that controls whether data is saved to disk (:code:`True`)
+            or only converted to a string and returned (:code:`False`), defaults to True
+        :type write_jsons_to_disk: bool, optional
+        :raises NotImplementedError: [description]
+        :return: filename, JSON(tween_params_dict) 
+            (either None if written to disk or a str)
+        :rtype: str, None/str
+        """
+
         if filename is None:
             filename = 'tweenParams.json'
         else:
