40215b4eca80f55f32ccf2e6ba1d64e7bc7245af
Alex Gurvich <agurvich@u.northwestern.edu>
2021-06-28 22:48:18 -0500

    document json_utils

--- src/Firefly/data_reader/json_utils.py
+++ src/Firefly/data_reader/json_utils.py
@@ -1,12 +1,31 @@
 import pandas as pd
 import os
 
-def write_to_json(dictionary,path):
-    return_value = pd.Series(dictionary)
-    return_value = return_value.to_json(path,orient='index')
-    return return_value
+def write_to_json(dictionary,path=None):
+    """Simple utility function to write a dictionary to a path as
+        as :code:`.json` file.
+        If path is None then instead a string is returned (thanks pandas!)
+
+    :param dictionary: arbitrary data dictionary that we want to output
+    :type dictionary: dict
+    :param path: filepath to output to, defaults to None
+    :type path: str
+    :return: JSON, either a string containing the JSON or the path the JSON was output to.
+    :rtype: str
+    """
+    JSON = pd.Series(dictionary)
+    JSON = return_value.to_json(path,orient='index')
+    if JSON is None: JSON = path
+    return JSON
 
 def load_from_json(path):
+    """Generate a python dictionary from a :code:`.json` that lives on disk.
+
+    :param path: filepath to input :code:`.json` file.
+    :type path: str
+    :return: dictionary
+    :rtype: dict
+    """
     if os.path.isfile(path):
         with open(path,'r') as handle:
             dictionary=pd.io.json.loads(''.join(handle.readlines()))
