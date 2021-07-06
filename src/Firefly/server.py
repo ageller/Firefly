1489b474d7a1cc9032e9cb93e9b4f2d4ac2fae18
Alex Gurvich <agurvich@u.northwestern.edu>
2021-07-06 13:42:55 -0500

    finish flask tutorial

--- src/Firefly/server.py
+++ src/Firefly/server.py
@@ -269,8 +269,10 @@ def spawnFireflyServer(*args):
     :return: subprocess.Popen
     :rtype: subprocess handler
     """
+
+    run_server = os.path.join(os.path.dirname(__file__),'run_server.py')
     ## args must be positional, they are 1) port 2) fps 3) decimation_factor
-    return subprocess.Popen(["python", __file__]+np.array(args).astype('str').tolist())
+    return subprocess.Popen(["python", run_server]+np.array(args).astype('str').tolist())
 
 def killAllFireflyServers(pid=None):
     """Kill python processes associated with hosting Flask web-servers.
