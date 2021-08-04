15770d01739b5dda145e93e7b5ab412439b3da45
Alex Gurvich <agurvich@u.northwestern.edu>
2021-08-04 09:38:46 -0400

    move flask sendToGUI and setViewerParams to misc/utils.js

--- src/Firefly/static/js/misc/utils.js
+++ src/Firefly/static/js/misc/utils.js
@@ -56,3 +56,39 @@ function setParamsFromURL(paramsObj){
 		paramsObj[k] = parseFloat(vars[k])
 	});
 }
+
+/* FLASK HELPER FUNCTIONS USED A TON */
+//function to send events to the GUI
+function sendToGUI(GUIInput){
+	if (viewerParams.usingSocket){
+		socketParams.socket.emit('gui_input',GUIInput);
+	} else {
+		setParams(GUIInput);
+	}
+}
+
+//a bit clunky...
+function setViewerParamByKey(args){
+	//first argument is always the value of the variable that you want to set
+	//the remaining values in args are how to reference the variable, going in order for object keys as they would be written to access the variable (see below)
+	var value = args[0];
+	var key1 = args[1];
+	if (args.length == 2) {
+		viewerParams[key1] = JSON.parse(JSON.stringify(value));
+	} else if (args.length == 3) {
+		var key2 = args[2];
+		viewerParams[key1][key2] = JSON.parse(JSON.stringify(value));
+	} else if (args.length == 4) {
+		var key2 = args[2];
+		var key3 = args[3];
+		viewerParams[key1][key2][key3] = JSON.parse(JSON.stringify(value));
+	} else if (args.length == 5) {
+		var key2 = args[2];
+		var key3 = args[3];
+		var key4 = args[4];
+		viewerParams[key1][key2][key3][key4] = JSON.parse(JSON.stringify(value));
+	} else {
+		console.log('!!!! WRONG NUMBER OF ARGUMENTS TO PASS', args.length, args)
+	}
+	//console.log(args)
+}
