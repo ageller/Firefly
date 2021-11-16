de38a7f7cb5b20b430a1711f917e8da13b15ce38
Aaron Geller <a-geller@northwestern.edu>
2021-11-16 16:23:17 -0600

    using same geo and mat functions for octree and normal

--- src/firefly/static/js/octree/octreeCreatePartsMesh.js
+++ src/firefly/static/js/octree/octreeCreatePartsMesh.js
@@ -1,121 +1,4 @@
-
-function createOctreeParticleGeometry(p, parts, start, end){
-	//geometry
-	var geo = new THREE.BufferGeometry();
-	//if all == true, then we draw all the particles except the first, which will always be there to define the node locations
-	//otherwise, we only draw the first particle	
-	if (!start) start = 0;
-	if (!end) end = parts.Coordinates.length;
-	end = Math.min(parts.Coordinates.length, end);
-	var len = end - start;
-	var i0 = start;
-	var id = name;
-
-	// attributes
-	//position
-	var position = new Float32Array( len*3 ); // 3 vertices per point
-	geo.addAttribute( 'position', new THREE.BufferAttribute( position, 3 ) );
-
-	//index
-	var pointIndex = new Float32Array( len );
-	geo.addAttribute( 'pointIndex', new THREE.BufferAttribute( pointIndex, 1 ) );
-
-	//radiusScaling (e.g., for filtering and on/off)
-	var radiusScale = new Float32Array( len ); 
-	geo.addAttribute( 'radiusScale', new THREE.BufferAttribute( radiusScale, 1 ) );
-
-	//alphas (e.g., for filtering and on/off)
-	var alpha = new Float32Array( len ); 
-	geo.addAttribute( 'alpha', new THREE.BufferAttribute( alpha, 1 ) );
-
-	//angles for velocities
-	var velVals = new Float32Array( len * 4); // unit vector (vx, vy, vz, norm), scaled magnitude
-	geo.addAttribute( 'velVals', new THREE.BufferAttribute( velVals, 4 ) );
-
-	//if user supplies individual per-particle colors (otherwise this is not used, but still need in shader)
-	if (viewerParams.parts[p].hasOwnProperty("colorArray")) console.log("have color Array")
-	var colorArray = new Float32Array( len * 4); // RGBA
-	geo.addAttribute( 'colorArray', new THREE.BufferAttribute( colorArray, 4 ) );
-	
-	// create array to hold colormap variable values
-	var colormapArray = new Float32Array( len); 
-	geo.addAttribute('colormapArray', new THREE.BufferAttribute( colormapArray, 1));
-
-	geo.setDrawRange( 0, len );
-
-	var pindex = 0;
-	var cindex = 0;
-	var colorIndex = 0;
-	var vindex = 0;
-	var rindex = 0;
-	var aindex = 0;
-
-	for (var j=0; j<len; j++){
-		position[pindex++] = parseFloat(parts.Coordinates[j][0]);
-		position[pindex++] = parseFloat(parts.Coordinates[j][1]);
-		position[pindex++] = parseFloat(parts.Coordinates[j][2]);
-
-		pointIndex[j] = parseFloat(j);
-
-		if (parts.hasOwnProperty("VelVals")){
-			velVals[vindex++] = parts.VelVals[j][0]/parts.magVelocities[j];
-			velVals[vindex++] = parts.VelVals[j][1]/parts.magVelocities[j];
-			velVals[vindex++] = parts.VelVals[j][2]/parts.magVelocities[j];
-			velVals[vindex++] = parts.NormVel[j];
-		}
-
-		// fill flattened color array from pre-computed colormap values
-		// stored in viewerParams.parts[p]["colorArray"]
-		//probably a better way to deal with this
-		if (parts.hasOwnProperty("colorArray")){
-			colorArray[colorIndex++] = parts.colorArray[j][0]
-			colorArray[colorIndex++] = parts.colorArray[j][1]
-			colorArray[colorIndex++] = parts.colorArray[j][2]
-			colorArray[colorIndex++] = parts.colorArray[j][3];
-		 } else {
-			colorArray[colorIndex++] = 0.;
-			colorArray[colorIndex++] = 0.;
-			colorArray[colorIndex++] = 0.;
-			colorArray[colorIndex++] = -1.;
-
-		}
-
-		// NEED TO UPDATE THIS
-		// fill colormap array with appropriate variable values
-		if (viewerParams.colormap[p] > 0.){
-			if (parts[viewerParams.ckeys[p][viewerParams.colormapVariable[p]]] != null){
-				colormapArray[cindex++] = parts[viewerParams.ckeys[p][viewerParams.colormapVariable[p]]][j];
-			}
-		}
-
-		var rad = 1.;
-		var alph = 1.;
-		if (!octreeParticleInFilter(p, parts, j)){	
-			rad = 0.;
-			alph = 0.;
-		}
-
-		if (parts.hasOwnProperty("SmoothingLength")){
-			radiusScale[rindex++] = parts.SmoothingLength[j]*rad;
-		}
-		else{
-			radiusScale[rindex++] = rad;
-		}
-		
-		alpha[aindex++] = alph;
-		
-		//need to do this somewhere else?
-		// ndraw += 1;
-		// if (ndraw % ndiv < 1 || ndraw == viewerParams.parts.totalSize){
-		// 	viewerParams.drawfrac = (1 + ndraw/viewerParams.parts.totalSize)*0.5;
-		// }
-
-	}
-
-	return geo;
-}
-
-function addOctreeParticlesToScene(p, parts, name, start, end, minPointSize=viewerParams.octree.defaultMinParticleSize, pointScale=1., updateGeo=false){
+function addOctreeParticlesToScene(p, parts, name, start, end, minPointSize=viewerParams.octree.defaultMinParticleSize, octreePointScale=1., updateGeo=false){
 	//I can use the start and end values to define how many particles to add to the mesh,
 	//  but first I want to try limitting this in the shader with maxToRender.  That may be quicker than add/removing meshes.
 
@@ -123,7 +6,7 @@ function addOctreeParticlesToScene(p, parts, name, start, end, minPointSize=view
 	if (end - start > 0){
 
 		//geometry
-		var geo = createOctreeParticleGeometry(p, parts, start, end);
+		var geo = createParticleGeometry(p, parts, start, end);
 
 		var maxN = end - start;
 
@@ -136,51 +19,9 @@ function addOctreeParticlesToScene(p, parts, name, start, end, minPointSize=view
 			}
 
 		} else {
-			//create the mesh
-			var blend = THREE.AdditiveBlending;
-			var dWrite = false;
-			var dTest = false;
-			var transp = true;
-			if (viewerParams.showColormap[p]){
-				blend = THREE.NormalBlending;
-				dWrite = true;
-				dTest = true;
-			}
-
-			var material = new THREE.ShaderMaterial( {
-
-
-				uniforms: { //add uniform variable here
-					color: {value: new THREE.Vector4( viewerParams.Pcolors[p][0], viewerParams.Pcolors[p][1], viewerParams.Pcolors[p][2], viewerParams.Pcolors[p][3])},
-					oID: {value: 0},
-					SPHrad: {value: viewerParams.parts[p].doSPHrad},
-					uVertexScale: {value: viewerParams.PsizeMult[p]},
-					maxDistance: {value: viewerParams.boxSize},
-					cameraY: {value: [0.,1.,0.]},
-					cameraX: {value: [1.,0.,0.]},
-					velType: {value: 0.},
-					colormapTexture: {value: viewerParams.colormapTexture},
-					colormap: {value: viewerParams.colormap[p]},
-					showColormap: {value: viewerParams.showColormap[p]},
-					colormapMin: {value: viewerParams.colormapVals[p][viewerParams.ckeys[p][viewerParams.colormapVariable[p]]][0]},
-					colormapMax: {value: viewerParams.colormapVals[p][viewerParams.ckeys[p][viewerParams.colormapVariable[p]]][1]},
-					columnDensity: {value: viewerParams.columnDensity},
-					scaleCD: {value: viewerParams.scaleCD},
-					minPointSize: {value: minPointSize},
-					maxToRender: {value: parts.Coordinates.length},
-					octreePointScale: {value: pointScale},
-
-				},
-
-				vertexShader: myVertexShader,
-				fragmentShader: myFragmentShader,
-				depthWrite:dWrite,
-				depthTest: dTest,
-				transparent:transp,
-				alphaTest: false,
-				blending:blend,
-			} );
-
+	
+			//material
+			var material = createParticleMaterial(p, parts.Coordinates.length, minPointSize, octreePointScale);
 
 			var mesh = new THREE.Points(geo, material);
 			mesh.name = name;
--- src/firefly/static/js/viewer/createPartsMesh.js
+++ src/firefly/static/js/viewer/createPartsMesh.js
@@ -13,8 +13,169 @@ function clearPartsMesh(pClear = viewerParams.partsKeys) {
 	}
 }
 
-function createPartsMesh(pdraw = viewerParams.partsKeys)
-{
+function createParticleGeometry(p, parts, start, end){
+
+	//geometry
+	var geo = new THREE.BufferGeometry();
+	//if all == true, then we draw all the particles except the first, which will always be there to define the node locations
+	//otherwise, we only draw the first particle	
+	if (!start) start = 0;
+	if (!end) end = parts.Coordinates.length;
+	end = Math.min(parts.Coordinates.length, end);
+	var len = end - start;
+	var i0 = start;
+	var id = name;
+
+	// attributes
+	//position
+	var position = new Float32Array( len*3 ); // 3 vertices per point
+	geo.addAttribute( 'position', new THREE.BufferAttribute( position, 3 ) );
+
+	//index
+	var pointIndex = new Float32Array( len );
+	geo.addAttribute( 'pointIndex', new THREE.BufferAttribute( pointIndex, 1 ) );
+
+	//radiusScaling (e.g., for filtering and on/off)
+	var radiusScale = new Float32Array( len ); 
+	geo.addAttribute( 'radiusScale', new THREE.BufferAttribute( radiusScale, 1 ) );
+
+	//alphas (e.g., for filtering and on/off)
+	var alpha = new Float32Array( len ); 
+	geo.addAttribute( 'alpha', new THREE.BufferAttribute( alpha, 1 ) );
+
+	//angles for velocities
+	var velVals = new Float32Array( len * 4); // unit vector (vx, vy, vz, norm), scaled magnitude
+	geo.addAttribute( 'velVals', new THREE.BufferAttribute( velVals, 4 ) );
+
+	//if user supplies individual per-particle colors (otherwise this is not used, but still need in shader)
+	if (viewerParams.parts[p].hasOwnProperty("colorArray")) console.log("have color Array")
+	var colorArray = new Float32Array( len * 4); // RGBA
+	geo.addAttribute( 'colorArray', new THREE.BufferAttribute( colorArray, 4 ) );
+	
+	// create array to hold colormap variable values
+	var colormapArray = new Float32Array( len); 
+	geo.addAttribute('colormapArray', new THREE.BufferAttribute( colormapArray, 1));
+
+	geo.setDrawRange( 0, len );
+
+	var pindex = 0;
+	var cindex = 0;
+	var colorIndex = 0;
+	var vindex = 0;
+	var rindex = 0;
+	var aindex = 0;
+
+	for (var j=0; j<len; j++){
+		position[pindex++] = parseFloat(parts.Coordinates[j][0]);
+		position[pindex++] = parseFloat(parts.Coordinates[j][1]);
+		position[pindex++] = parseFloat(parts.Coordinates[j][2]);
+
+		pointIndex[j] = parseFloat(j);
+
+		if (parts.hasOwnProperty("VelVals")){
+			velVals[vindex++] = parts.VelVals[j][0]/parts.magVelocities[j];
+			velVals[vindex++] = parts.VelVals[j][1]/parts.magVelocities[j];
+			velVals[vindex++] = parts.VelVals[j][2]/parts.magVelocities[j];
+			velVals[vindex++] = parts.NormVel[j];
+		}
+
+		// fill flattened color array from pre-computed colormap values
+		// stored in viewerParams.parts[p]["colorArray"]
+		//probably a better way to deal with this
+		if (parts.hasOwnProperty("colorArray")){
+			colorArray[colorIndex++] = parts.colorArray[j][0]
+			colorArray[colorIndex++] = parts.colorArray[j][1]
+			colorArray[colorIndex++] = parts.colorArray[j][2]
+			colorArray[colorIndex++] = parts.colorArray[j][3];
+		 } else {
+			colorArray[colorIndex++] = 0.;
+			colorArray[colorIndex++] = 0.;
+			colorArray[colorIndex++] = 0.;
+			colorArray[colorIndex++] = -1.;
+
+		}
+
+		// NEED TO UPDATE THIS
+		// fill colormap array with appropriate variable values
+		if (viewerParams.colormap[p] > 0.){
+			if (parts[viewerParams.ckeys[p][viewerParams.colormapVariable[p]]] != null){
+				colormapArray[cindex++] = parts[viewerParams.ckeys[p][viewerParams.colormapVariable[p]]][j];
+			}
+		}
+
+		var rad = 1.;
+		var alph = 1.;
+		if (!octreeParticleInFilter(p, parts, j)){	
+			rad = 0.;
+			alph = 0.;
+		}
+
+		if (parts.hasOwnProperty("SmoothingLength")){
+			radiusScale[rindex++] = parts.SmoothingLength[j]*rad;
+		}
+		else{
+			radiusScale[rindex++] = rad;
+		}
+		
+		alpha[aindex++] = alph;
+
+	}
+
+	return geo;
+}
+
+function createParticleMaterial(p, maxToRender, minPointSize=1, octreePointScale=1.){
+	//change the blending mode when showing the colormap (so we don't get summing to white colors)
+	var blend = THREE.AdditiveBlending;
+	var dWrite = false;
+	var dTest = false;
+	var transp = true;
+	if (viewerParams.showColormap[p]){
+		blend = THREE.NormalBlending;
+		dWrite = true;
+		dTest = true;
+	}
+
+	var material = new THREE.ShaderMaterial( {
+
+
+		uniforms: { //add uniform variable here
+			color: {value: new THREE.Vector4( viewerParams.Pcolors[p][0], viewerParams.Pcolors[p][1], viewerParams.Pcolors[p][2], viewerParams.Pcolors[p][3])},
+			oID: {value: 0},
+			SPHrad: {value: viewerParams.parts[p].doSPHrad},
+			uVertexScale: {value: viewerParams.PsizeMult[p]},
+			maxDistance: {value: viewerParams.boxSize},
+			cameraY: {value: [0.,1.,0.]},
+			cameraX: {value: [1.,0.,0.]},
+			velType: {value: 0.},
+			colormapTexture: {value: viewerParams.colormapTexture},
+			colormap: {value: viewerParams.colormap[p]},
+			showColormap: {value: viewerParams.showColormap[p]},
+			colormapMin: {value: viewerParams.colormapVals[p][viewerParams.ckeys[p][viewerParams.colormapVariable[p]]][0]},
+			colormapMax: {value: viewerParams.colormapVals[p][viewerParams.ckeys[p][viewerParams.colormapVariable[p]]][1]},
+			columnDensity: {value: viewerParams.columnDensity},
+			scaleCD: {value: viewerParams.scaleCD},
+			minPointSize: {value: minPointSize},
+			maxToRender: {value: maxToRender},
+			octreePointScale: {value: octreePointScale},
+
+		},
+
+		vertexShader: myVertexShader,
+		fragmentShader: myFragmentShader,
+		depthWrite:dWrite,
+		depthTest: dTest,
+		transparent:transp,
+		alphaTest: false,
+		blending:blend,
+	} );
+
+
+	return material;
+}
+
+function createPartsMesh(pdraw = viewerParams.partsKeys, node=null){
+	
 	clearPartsMesh(pClear = pdraw);
 	console.log("drawing", pdraw, viewerParams.plotNmax,viewerParams.decimate)
 
@@ -34,202 +195,26 @@ function createPartsMesh(pdraw = viewerParams.partsKeys)
 
 		viewerParams.partsMesh[p] = [];
 	
-		//change the blending mode when showing the colormap (so we don't get summing to white colors)
-		var blend = THREE.AdditiveBlending;
-		var dWrite = false;
-		var dTest = false;
-		var transp = true;
-
-		if (viewerParams.showColormap[p]){
-			blend = THREE.NormalBlending;
-			dWrite = true;
-			dTest = true;
-			transp = true; //still need this because I use alpha to set control filtering!
-		}
-
-		var material = new THREE.ShaderMaterial( {
-
-			uniforms: { //add uniform variable here
-				color: {value: new THREE.Vector4( viewerParams.Pcolors[p][0], viewerParams.Pcolors[p][1], viewerParams.Pcolors[p][2], viewerParams.Pcolors[p][3])},
-				oID: {value: 0},
-				SPHrad: {value: viewerParams.parts[p].doSPHrad},
-				uVertexScale: {value: viewerParams.PsizeMult[p]},
-				maxDistance: {value: viewerParams.boxSize},
-				cameraY: {value: [0.,1.,0.]},
-				cameraX: {value: [1.,0.,0.]},
-				velType: {value: 0.},
-				colormapTexture: {value: viewerParams.colormapTexture},
-				colormap: {value: viewerParams.colormap[p]},
-				showColormap: {value: viewerParams.showColormap[p]},
-				colormapMin: {value: viewerParams.colormapVals[p][viewerParams.ckeys[p][viewerParams.colormapVariable[p]]][0]},
-				colormapMax: {value: viewerParams.colormapVals[p][viewerParams.ckeys[p][viewerParams.colormapVariable[p]]][1]},
-				columnDensity: {value: viewerParams.columnDensity},
-				scaleCD: {value: viewerParams.scaleCD},
-			},
-
-			vertexShader: myVertexShader,
-			fragmentShader: myFragmentShader,
-			depthWrite:dWrite,
-			depthTest: dTest,
-			transparent:transp,
-			alphaTest: false,
-			blending:blend,
-		} );
-
 		//geometry
-		//var geo = new THREE.Geometry();
-		var geo = new THREE.BufferGeometry();
+		var geo = createParticleGeometry(p, viewerParams.parts[p], 0, viewerParams.plotNmax[p]);
 
-		// attributes
-		//positions
-		var positions = new Float32Array( viewerParams.plotNmax[p] * 3 ); // 3 vertices per point
-		geo.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-
-		//radiusScaling (e.g., for filtering and on/off)
-		var radiusScale = new Float32Array( viewerParams.plotNmax[p] ); 
-		geo.addAttribute( 'radiusScale', new THREE.BufferAttribute( radiusScale, 1 ) );
-
-		//alphas (e.g., for filtering and on/off)
-		var alpha = new Float32Array( viewerParams.plotNmax[p] ); 
-		geo.addAttribute( 'alpha', new THREE.BufferAttribute( alpha, 1 ) );
-
-		//angles for velocities
-		var velVals = new Float32Array( viewerParams.plotNmax[p] * 4); // unit vector (vx, vy, vz), scaled magnitude
-		geo.addAttribute( 'velVals', new THREE.BufferAttribute( velVals, 4 ) );
-
-		//if user supplies individual per-particle colors (otherwise this is not used, but still need in shader)
-		if (viewerParams.parts[p].hasOwnProperty("colorArray")) console.log("have color Array")
-		var colorArray = new Float32Array( viewerParams.plotNmax[p] * 4); // RGBA
-		geo.addAttribute( 'colorArray', new THREE.BufferAttribute( colorArray, 4 ) );
-		
-		// create array to hold colormap variable values
-		var colormapArray = new Float32Array( viewerParams.plotNmax[p]); 
-		geo.addAttribute('colormapArray', new THREE.BufferAttribute( colormapArray, 1));
-
-		geo.setDrawRange( 0, viewerParams.plotNmax[p] );
+		//material
+		var material = createParticleMaterial(p, viewerParams.plotNmax[p]);
 
 		var mesh = new THREE.Points(geo, material);
-		mesh.name = p;
-		viewerParams.scene.add(mesh);
-
-
-
-		//var positions = mesh.geometry.attributes.position.array;
-		var cindex = 0;
-		var colorIndex = 0;
-		var pindex = 0;
-		var vindex = 0;
-		var rindex = 0;
-		var aindex = 0;
-
-		var includePoint = true;
-		//for (var j=0; j<viewerParams.parts[p].Coordinates.length/viewerParams.decimate; j++){
-		for (var j=0; j<viewerParams.plotNmax[p]; j++){
-
-			includePoint = true;
-			//if we redraw upon filtering, then we would include the filtering here 
-			// for (k=0; k<viewerParams.fkeys[p].length; k++){
-			// 	if (viewerParams.parts[p][viewerParams.fkeys[p][k]] != null) {
-			// 		val = viewerParams.parts[p][viewerParams.fkeys[p][k]][j]; 
-			// 		if ( val < viewerParams.filterVals[p][viewerParams.fkeys[p][k]][0] || val > viewerParams.filterVals[p][viewerParams.fkeys[p][k]][1] ){
-			// 			includePoint = false;
-			// 		} 
-			// 	}
-			// }
-
-			if (includePoint){
-
-				//geo.vertices.push(new THREE.Vector3(viewerParams.parts[p].Coordinates[j][0], viewerParams.parts[p].Coordinates[j][1], viewerParams.parts[p].Coordinates[j][2] ))
-				
-				positions[pindex] = viewerParams.parts[p].Coordinates[j][0];
-				pindex++;
-				positions[pindex] = viewerParams.parts[p].Coordinates[j][1];
-				pindex++;
-				positions[pindex] = viewerParams.parts[p].Coordinates[j][2];
-				pindex++;
-
-
-				if (viewerParams.parts[p].Velocities != null){
-					velVals[vindex] = viewerParams.parts[p].VelVals[j][0]/viewerParams.parts[p].magVelocities[j];
-					vindex++;
-					velVals[vindex] = viewerParams.parts[p].VelVals[j][1]/viewerParams.parts[p].magVelocities[j];
-					vindex++;
-					velVals[vindex] = viewerParams.parts[p].VelVals[j][2]/viewerParams.parts[p].magVelocities[j];
-					vindex++;
-					velVals[vindex] = viewerParams.parts[p].NormVel[j];
-					vindex++;
-				}
-
-				// fill flattened color array from pre-computed colormap values
-				// stored in viewerParams.parts[p]["colorArray"]
-				//probably a better way to deal with this
-				if (viewerParams.parts[p].hasOwnProperty("colorArray")){
-					colorArray[colorIndex] = viewerParams.parts[p].colorArray[j][0]
-					colorIndex++;
-					colorArray[colorIndex] = viewerParams.parts[p].colorArray[j][1]
-					colorIndex++;
-					colorArray[colorIndex] = viewerParams.parts[p].colorArray[j][2]
-					colorIndex++;
-					colorArray[colorIndex] = viewerParams.parts[p].colorArray[j][3];
-					colorIndex++;
-				 } else {
-					colorArray[colorIndex] = 0.;
-					colorIndex++;
-					colorArray[colorIndex] = 0.;
-					colorIndex++;
-					colorArray[colorIndex] = 0.;
-					colorIndex++;
-					colorArray[colorIndex] = -1.;
-					colorIndex++;
-
-				}
-
-				// fill colormap array with appropriate variable values
-				if (viewerParams.colormap[p] > 0.){
-					if (viewerParams.parts[p][viewerParams.ckeys[p][viewerParams.colormapVariable[p]]] != null){
-						colormapArray[cindex] = viewerParams.parts[p][viewerParams.ckeys[p][viewerParams.colormapVariable[p]]][j];
-						cindex++;
-					}
-				}
-
-                if ('SmoothingLength' in viewerParams.parts[p]){
-                    radiusScale[rindex] = viewerParams.parts[p].SmoothingLength[j];
-                }
-                else{
-                    radiusScale[rindex] = 1.;
-                }
-				rindex++;
-				
-				alpha[aindex] = 1.;
-				aindex++;
-				
-				ndraw += 1;
-				if (ndraw % ndiv < 1 || ndraw == viewerParams.parts.totalSize){
-					viewerParams.drawfrac = (1 + ndraw/viewerParams.parts.totalSize)*0.5;
-					//updateDrawingBar();
-				}
-
-				//add to the octree -- I will only want to do this in the initial draw! (and do I need to remove objects during filtering?)
-				//viewerParams.octree.add({x:viewerParams.parts[p].Coordinates[j][0],
-				//						 y:viewerParams.parts[p].Coordinates[j][1],
-				//						 z:viewerParams.parts[p].Coordinates[j][2],
-				//						 radius:radiusScale[rindex],
-				//						 id:p+ndraw}); //probably don't want to initialize this every time
-
-			}
-		}
-
+		mesh.name = p + 'Standard';
 		mesh.position.set(0,0,0);
 
+		viewerParams.scene.add(mesh);
 		viewerParams.partsMesh[p].push(mesh)
 	}
 
 	//this will not be printed if you change the N value in the slider, and therefore only redraw one particle type
 	//because ndraw will not be large enough, but I don't think this will cause a problem
 	//if (ndraw >= Math.floor(viewerParams.parts.totalSize/viewerParams.decimate)){
-		console.log("done drawing")
+	console.log("done drawing")
 
-		clearloading();
+	clearloading();
 
 	//}
 	return true;
