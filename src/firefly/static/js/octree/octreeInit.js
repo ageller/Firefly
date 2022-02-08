8315bb12b6d0a812c3ba0574f0b6e7127f81e7ba
Alex Gurvich <agurvich@u.northwestern.edu>
2022-02-08 14:53:20 -0600

    move code into octreeInit

--- src/firefly/static/js/octree/octreeInit.js
+++ src/firefly/static/js/octree/octreeInit.js
@@ -2,7 +2,15 @@ function abg_initOctree(data){
 	// setup the open/close flags for each node
 	evaluateFunctionOnOctreeNodes(
 		initializeNodeFlags,
-		data.octree[''], data.octree);
+		data.octree[''],
+		data.octree);
+
+	viewerParams.debug = true;
+	// initialize octree boxes
+	evaluateFunctionOnOctreeNodes(
+		createOctBox,
+		data.octree[''],
+		data.octree);
 }
 
 function initializeNodeFlags(node){
@@ -10,6 +18,60 @@ function initializeNodeFlags(node){
 	node.is_open = false
 	node.delay_close = 0
 	node.delay_open = 0
+}
+
+function createOctBox(node){
+	if (viewerParams.debug) {
+		const geometry = new THREE.BufferGeometry();
+		// create a simple square shape. We duplicate the top left and bottom right
+		// vertices because each vertex needs to appear once per triangle.
+		const vertices = new Float32Array( [
+			-1.0, -1.0,  1.0,
+			1.0, -1.0,  1.0,
+			1.0,  1.0,  1.0,
+
+			1.0,  1.0,  1.0,
+			-1.0,  1.0,  1.0,
+			-1.0, -1.0,  1.0,
+
+			1.0,  1.0,  -1.0,
+			-1.0,  1.0,  -1.0,
+			-1.0, -1.0,  -1.0
+		] );
+
+		// itemSize = 3 because there are 3 values (components) per vertex
+		geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+		const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+		const mesh = new THREE.Mesh( geometry, material );
+		const wireframe = new THREE.WireframeGeometry( mesh.geometry );
+		let line = new THREE.LineSegments( wireframe );
+		line.material.depthTest = false;
+		line.material.opacity = 0.25;
+		line.material.transparent = true;
+		line.position.x = node.center[0];
+		line.position.y = node.center[1];
+		line.position.z = node.center[2];
+		line.scale.x = line.scale.y = line.scale.z = node.width/2;
+
+		var obj =  new THREE.BoxHelper( line );
+		obj.visible=true;
+		node.octbox = obj;
+	}
+
+	upper = new THREE.Vector3(
+		node.center[0] + node.width/2.,
+		node.center[1] + node.width/2.,
+		node.center[2] + node.width/2.);
+
+	lower = new THREE.Vector3(
+		node.center[0] - node.width/2.,
+		node.center[1] - node.width/2.,
+		node.center[2] - node.width/2.);
+
+	const bounding_box = new THREE.Box3(lower,upper)
+	node.bounding_box = bounding_box
+
+	return obj;
 }
 
 function initNode(node, p){
