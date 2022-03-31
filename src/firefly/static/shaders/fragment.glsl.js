1c4e7f16d3b9d8b308f129f404a00cdbde0bed0f
Alex Gurvich <agurvich@u.northwestern.edu>
2022-03-31 15:32:52 -0500

    rever change to fragment discarding

--- src/firefly/static/shaders/fragment.glsl.js
+++ src/firefly/static/shaders/fragment.glsl.js
@@ -56,8 +56,10 @@ void main(void) {
 		float dist2 = dist*dist;
 		// fix for the minimum point size imposed by WebGL context gl.ALIASED_POINT_SIZE_RANGE = [1, ~8000]
 		float dMax = min(1., vPointSize);
-		if (dist > dMax){
-			discard;
+		if (useDepth > 0.5){
+			if (dist > dMax){
+				discard;
+			}
 		} else {
 			if (SPHrad == 1){
 				// best fit quartic to SPH kernel (unormalized)
