769db25efd99c2bf1849b2b421f41c07b9b520b7
Alex Gurvich <agurvich@u.northwestern.edu>
2022-06-13 23:23:58 -0500

    add comment for reversing colormap. one day i will come back for you little comment.

--- src/firefly/static/shaders/fragment.glsl.js
+++ src/firefly/static/shaders/fragment.glsl.js
@@ -51,6 +51,7 @@ void main(void) {
 		// if colormap is requested, apply appropriate colormap to appropriate variable
 		//  this should have priority over everything else (so long)
 		else if (showColormap){
+			// if you want to reverse colormap: vec2 pos = vec2(1.-vColormapMag, colormap);
 			vec2 pos = vec2(vColormapMag, colormap);
 			vec3 c = texture2D(colormapTexture, pos).rgb;
 			gl_FragColor.rgb = c;
