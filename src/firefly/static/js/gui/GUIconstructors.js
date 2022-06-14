3914811c4b8975edadae8a48ba6812c35af922e5
Alex Gurvich <agurvich@u.northwestern.edu>
2022-06-13 23:10:38 -0500

    Revert "disable colormap toggling when projection is enabled"
    This reverts commit cadfc96ea810c7f25ba47c033b479c2adb7db6aa.
    
    don't need to do this now that i set the color manually in the fragment shader and
    ignore colormapping if columnDensity is true.

--- src/firefly/static/js/gui/GUIconstructors.js
+++ src/firefly/static/js/gui/GUIconstructors.js
@@ -1056,10 +1056,8 @@ function createParticleColormapCheckBoxSegment(container,parent,name,p){
 		.attr('type','checkbox')
 		.attr('autocomplete','off')
 		.on('change',function(){
-			if (!GUIParams.columnDensity){
-				if (GUIParams.showParts[p]) checkColormapBox(p, this.checked);
-				else this.checked = GUIParams.showColormap[p];
-			}
+			if (GUIParams.showParts[p]) checkColormapBox(p, this.checked);
+			else this.checked = GUIParams.showColormap[p];
 		})
 	this_container.append('label')
 		.attr('for',p+'colorCheckBox')
