e21e3318fc93c04f863e1cd7c57ea9fbd98c8733
Alex Gurvich <agurvich@u.northwestern.edu>
2022-07-03 09:07:57 -0500

    * move out startHTTPServer to firefly.server (makes more sense) * add more sophisticated arg parsing * add directory switch to serve Firefly directories that aren't in the python distribution directory

--- src/firefly/run_server.py
+++ src/firefly/run_server.py
@@ -1,51 +1,63 @@
 #!/usr/bin/env python
 import sys
 import os
-import http.server
-import socketserver
+import getopt
 
 ## make sure we are importing from wherever this file is, rather than the system 
 ##  installation of Firefly. Saves us devs a lot of confusion and is equivalent 
 ##  if you're only using a pip installed version
 sys.path.insert(0,os.path.abspath(os.path.join(os.getcwd(),'..')))
-from firefly.server import startFireflyServer
+from firefly.server import startHTTPServer, startFlaskServer
 
-def startHTTPServer(port=5500):
-    Handler = http.server.SimpleHTTPRequestHandler
-    with socketserver.TCPServer(("", port), Handler) as httpd:
-        ipadd,port=httpd.server_address
-        os.chdir(os.path.dirname(os.path.dirname(__file__)))
-        print(f'Serving {os.getcwd()}/index.html at http://{ipadd}:{port}')
-        httpd.serve_forever()
+def main(
+    port=5500,
+    method='http',
+    directory=None,
+    dec=1,
+    fps=30):
+    """Creates a global interpreter locked process to host either a Flask 
+        or HTTP server that can be accessed via localhost:<port>. 
 
-def main():
-    #app.run(host='0.0.0.0')
+    :param port: port number to serve the :code:`.html` files on, defaults to 5500
+    :type port: int, optional
+    :param method: what sort of Firefly server to open, a Flask ("flask") server 
+        or an HTTP ("http"), defaults to "flask"
+    :type method: str, optional
+    :param directory: the directory of the Firefly source files to be served, 
+        if None, uses `os.dirname(__file__)` i.e. the directory of the `firefly`
+        python distribution, defaults to None
+    :type directory: str, optional
+    :param fps: enforced FPS for stream quality, used only if
+		localhost:<port>/stream is accessed, defaults to 30
+    :type fps: int, optional
+    :param dec: factor to decimate data that is being passed through
+		localhost:<port>/data_input, defaults to 1
+    :type dec: int, optional
+    """
 
-    #Note: we could have a more sophisticated arg parser, but this is probably fine for now.
-    #port as the first input
-    args = sys.argv[1:]
+    if method not in ['flask','http']: raise ValueError(
+        f"method must be one of flask or http, not {method}")
 
-    if len(args) >=1: 
-        method = args[0]
-        if method not in ['flask','http']: raise ValueError(
-            f"second argument method must be one of flask or http, not {method}")
-    else: method = 'http'
+    if method == 'flask': startFlaskServer(port,directory,fps,dec)
+    else: startHTTPServer(port,directory)
 
-    if len(args) >=2: port = int(args[1])
-    else: port = 5500
-
-    #stream fps as a second input
-    if len(args) >=3: fps = float(args[2])
-    else: fps = 30
-
-    #decimation factor as third input (for reading in files)
-    if len(args) >=4: dec = float(args[3])
-    else: dec = 1
-
-    #socketio.run(app, debug=True, host='0.0.0.0', port=port)
-    if method == 'flask': startFireflyServer(port=port, frames_per_second=fps, decimation_factor=dec)
-    else: startHTTPServer(port)
-
-if __name__ == "__main__":
-    main()
-    
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    opts,args = getopt.getopt(
+        argv,'',[
+        'method=',
+        'port=',
+        'directory=',
+        'dec=',
+        'fps='])
+    for i,opt in enumerate(opts):
+        if opt[1]=='':
+            opts[i]=('mode',opt[0].replace('-',''))
+        else:
+            try:
+                ## if it's an int or a float this should work
+                opts[i]=(opt[0].replace('-',''),eval(opt[1]))
+            except:
+                ## if it's a string... not so much
+                opts[i]=(opt[0].replace('-',''),opt[1])
+    main(**dict(opts))
