8adb0d900fc4db3b22c5d69131e1d99ca05ab51e
ageller <a-geller@northwestern.edu>
2024-10-03 14:30:18 -0500

    removed comment

--- src/firefly/server.py
+++ src/firefly/server.py
@@ -409,7 +409,6 @@ def reload():
 
 @app.after_request
 def add_header(response):
-    print('TESTING flask')
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '0'
