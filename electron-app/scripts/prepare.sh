b3ec59a88f1cc8fc0981ca87102334897aea8c34
ageller <a-geller@northwestern.edu>
2025-08-26 17:02:42 -0500

    modifying for windows

--- electron-app/scripts/prepare.sh
+++ electron-app/scripts/prepare.sh
@@ -15,7 +15,7 @@ elif [ -x "./bundle/python/python.exe" ]; then
 else
     echo "WARNING!!! Cannot automatically find python executable.  This script will not be able to install the default Firefly data.  Please modify the prepare.sh script to fix this."
 fi
-SITE_PACKAGES=$("$PYTHON" -c "import site; print(site.getsitepackages()[0])")
+SITE_PACKAGES=$("$PYTHON" -c "import site; print(site.getsitepackages()[-1])")
 cp -r ../src/firefly/static/data/FIRESampleData "$SITE_PACKAGES/firefly/static/data/"
 
 # copy the notebooks to the bundle specifically so the user isn't working inside the site-packages dir
