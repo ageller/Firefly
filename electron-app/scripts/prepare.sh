42c43e9d402ef5de32ca8881542c8c26ceffa40d
ageller <a-geller@northwestern.edu>
2025-08-26 15:57:36 -0500

    attempting fix for Windows python path in prepare.sh

--- electron-app/scripts/prepare.sh
+++ electron-app/scripts/prepare.sh
@@ -8,7 +8,14 @@ conda activate ./bundle/python
 pip install ../ jupyter
 
 # copy the Firefly data to the bundle so that it loads by default
-SITE_PACKAGES=$(./bundle/python/bin/python -c "import site; print(site.getsitepackages()[0])")
+if [ -x "./bundle/python/bin/python" ]; then
+    PYTHON="./bundle/python/bin/python"
+elif [ -x "./bundle/python/python.exe" ]; then
+    PYTHON="./bundle/python/python.exe"
+else
+    echo "WARNING!!! Cannot automatically find python executable.  This script will not be able to install the default Firefly data.  Please modify the prepare.sh script to fix this."
+fi
+SITE_PACKAGES=$("$PYTHON" -c "import site; print(site.getsitepackages()[0])")
 cp -r ../src/firefly/static/data/FIRESampleData "$SITE_PACKAGES/firefly/static/data/"
 
 # copy the notebooks to the bundle specifically so the user isn't working inside the site-packages dir
